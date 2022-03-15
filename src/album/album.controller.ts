b9c216e7893a4efcb7a3f0dbc6c513d0c807c55c
Sergio Sierra <sd.sierra@uniandes.edu.co>
2022-03-15 18:56:56 -0500

    jwt done

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,38 +1,50 @@
-import { Controller, Get, Post, HttpCode, Body, Param, Put, Delete, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  Body,
+  Param,
+  Put,
+  Delete,
+  UseInterceptors,
+  UseGuards,
+} from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumDTO } from './album.dto';
 
-import { BusinessErrorsInterceptor } from "../interceptors/interceptor";
+import { BusinessErrorsInterceptor } from '../interceptors/interceptor';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 @Controller('albums')
 @UseInterceptors(BusinessErrorsInterceptor)
+@UseGuards(JwtAuthGuard)
 export class AlbumController {
-    constructor(private readonly albumService: AlbumService) { }
+  constructor(private readonly albumService: AlbumService) {}
+  @Get()
+  async findAll() {
+    return await this.albumService.findAll();
+  }
 
-    @Get()
-    async findAll() {
-        return await this.albumService.findAll();
-    }
+  @Get(':albumId')
+  async findOne(@Param('albumId') albumId: number) {
+    return await this.albumService.findOne(albumId);
+  }
 
-    @Get(':albumId')
-    async findOne(@Param('albumId') albumId: number) {
-        return await this.albumService.findOne(albumId);
-    }
+  @Post()
+  @HttpCode(200)
+  async create(@Body() albumDTO: AlbumDTO) {
+    return await this.albumService.create(albumDTO);
+  }
 
-    @Post()
-    @HttpCode(200)
-    async create(@Body() albumDTO: AlbumDTO) {
-        return await this.albumService.create(albumDTO);
-    }
+  @Put(':albumId')
+  async update(@Param('albumId') albumId: number, @Body() albumDTO: AlbumDTO) {
+    return await this.albumService.update(albumId, albumDTO);
+  }
 
-    @Put(':albumId')
-    async update(@Param('albumId') albumId: number, @Body() albumDTO: AlbumDTO) {
-        return await this.albumService.update(albumId, albumDTO);
-    }
-
-    @Delete(':albumId')
-    @HttpCode(204)
-    async delete(@Param('albumId') albumId: number) {
-        return await this.albumService.delete(albumId)
-    }
-
+  @Delete(':albumId')
+  @HttpCode(204)
+  async delete(@Param('albumId') albumId: number) {
+    return await this.albumService.delete(albumId);
+  }
 }
